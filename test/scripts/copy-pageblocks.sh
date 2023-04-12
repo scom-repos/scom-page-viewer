80602fb048db70514ccbd7d859e731532688bb30
ojochau <ojochau@instajs.com>
2023-04-12 17:46:06 +0800

    update test folder

---
+++ test/scripts/copy-pageblocks.sh
@@ -0,0 +1,11 @@
+mkdir dist/libs/@scom/scom-dapp-container && cp -r node_modules/@scom/scom-dapp-container/dist/* dist/libs/@scom/scom-dapp-container && 
+mkdir dist/libs/@scom/scom-markdown-editor && cp -r node_modules/@scom/scom-markdown-editor/dist/* dist/libs/@scom/scom-markdown-editor && 
+mkdir dist/libs/@scom/scom-image && cp -r node_modules/@scom/scom-image/dist/* dist/libs/@scom/scom-image &&
+mkdir dist/libs/@scom/scom-carousel && cp -r node_modules/@scom/scom-carousel/dist/* dist/libs/@scom/scom-carousel &&
+mkdir dist/libs/@scom/scom-banner && cp -r node_modules/@scom/scom-banner/dist/* dist/libs/@scom/scom-banner &&
+mkdir dist/libs/@scom/scom-blog && cp -r node_modules/@scom/scom-blog/dist/* dist/libs/@scom/scom-blog &&
+mkdir dist/libs/@scom/scom-nft-minter && cp -r node_modules/@scom/scom-nft-minter/dist/* dist/libs/@scom/scom-nft-minter && 
+mkdir dist/libs/@scom/scom-gem-token && cp -r node_modules/@scom/scom-gem-token/dist/* dist/libs/@scom/scom-gem-token && 
+mkdir dist/libs/@scom/scom-randomizer && cp -r node_modules/@scom/scom-randomizer/dist/* dist/libs/@scom/scom-randomizer && 
+mkdir dist/libs/@scom/scom-video && cp -r node_modules/@scom/scom-video/dist/* dist/libs/@scom/scom-video &&
+mkdir dist/libs/@scom/scom-map && cp -r node_modules/@scom/scom-map/dist/* dist/libs/@scom/scom-map
